db3f87faa27905b509b8e4b1f93ca27483ed45a7
kirti-krishnn <kirti.krishnn@gmail.com>
2023-05-31 22:26:09 +0530

    working code

--- NumberGuessing/script.js
+++ NumberGuessing/script.js
@@ -8,9 +8,9 @@ class GuessingGame {
     console.log('k1');
   }
 
-  updateDOM(boolval, numwidth, bgcolor, guessval) {
+  updateDOM(boolval, msg, numwidth, bgcolor, guessval) {
     console.log('k2');
-    document.getElementById('guess').disabled = boolval;
+    document.querySelector('.guess').disabled = boolval;
     this.updateMsg(msg);
     document.querySelector('.number').style.width = numwidth;
     document.querySelector('body').style.backgroundColor = bgcolor;
@@ -29,19 +29,21 @@ class GuessingGame {
   }
   compareGuess(guess) {
     console.log('k5');
+    let msg;
     if (guess != this.number) {
       guess < this.number
         ? (msg = 'guess is too Low')
         : (msg = 'guess is too high');
       this.updateMsg(msg);
       this.score--;
-      if (score == 0) {
+      if (this.score == 0) {
         msg = 'you lost!! play again';
         this.updateMsg(msg);
-        document.getElementById('guess').disabled = true;
+        document.querySelector('.guess').disabled = true;
       }
     } else {
-      msg = 'congratulations you got it right';
+      msg =
+        'congratulations you got it right.Want to play again, Click Again button';
       this.updateHighScore(this.score);
       this.updateDOM(true, msg, '30rem', '#038200', guess);
     }
@@ -49,28 +51,28 @@ class GuessingGame {
   getUserValue() {
     console.log('k6');
     const val = document.querySelector('.guess').value;
-    let msg;
+
     if (val != '') {
       this.compareGuess(val);
     } else {
-      msg = 'Please enter a value';
+      let msg = 'Please enter a value';
       this.updateMsg(msg);
     }
   }
   reset() {
     console.log('k7');
-    score = 20;
-    guessNumber = Math.trunc(Math.random() * 20);
-    msg = 'you are right. Want to play again, Click Again button';
+    this.score = 20;
+    this.number = Math.trunc(Math.random() * 20);
+    let msg = 'Start Guessing ...';
     document.querySelector('.score').textContent = '20';
-    document.querySelector('#guess').value = '';
+    document.querySelector('.guess').value = '';
     this.updateDOM(false, msg, '15rem', '#333', '?');
   }
 }
 const game = new GuessingGame();
-game.updateHighScore(76);
+/* game.updateHighScore(76);
 //game.updateDOM(1, 2, 3, 4);
-game.getUserValue();
+game.getUserValue(); */
 document
   .querySelector('.check')
   .addEventListener('click', game.getUserValue.bind(game));
